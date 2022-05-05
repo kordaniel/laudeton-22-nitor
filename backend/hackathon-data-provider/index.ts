d529b9432838aa5d609255e9402bf37d76145a54
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2022-05-05 13:55:48 +0300

    Add REST API POST ./activities endpoint

--- backend/hackathon-data-provider/index.ts
+++ backend/hackathon-data-provider/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import Rand from 'rand-seed';
-import { fileHandler } from "./src/fileHandler";
+import { fileHandler, fileWriter } from "./src/fileHandler";
 import { User } from "./src/types";
 import { randomElementSeed } from "./src/util";
 import { getCoordinates } from "./src/coordinates";
 const app = express();
+
+app.use(express.json())
 
 const PORT = 3000;
 
@@ -12,6 +14,25 @@ function setHeaders(res : express.Response) {
     res.header("Content-Type",'application/json')
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Cache-Control", "private, max-age=0")
+}
+
+function generateId(objects) {
+    const jsonObjects = JSON.parse(objects)
+    const idLetters = 1;
+    const idNums    = 1;
+    const letters   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers   = '0123456789';
+    const lettersLength = letters.length;
+    const numbersLength = numbers.length;
+
+    var newId = 'A1';
+    while (jsonObjects.filter(o => o.id === newId).length > 0) {
+        newId = '';
+        newId += letters.charAt(Math.floor(Math.random() * lettersLength));
+        newId += numbers.charAt(Math.floor(Math.random() * numbersLength));
+    }
+
+    return newId;
 }
 
 app.get('/me',  async (req, res) => {
@@ -63,6 +84,28 @@ app.options('/me',  async (req, res) => {
 app.get('/robots.txt',  async (req, res) => {
     res.header("Content-Type",'text/plain');
     res.send('User-agent: *\nDisallow: /')
+})
+
+app.post('/activities', async (req, res) => {
+    const activities = await fileHandler("./data/activities.json");
+    const newId = generateId(activities);
+
+    const body = req.body
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'Activity name is missing'
+        })
+    }
+    if (!body.coordinates) {
+        return res.status(400).json({
+            error: 'Activity coordinates are missing'
+        })
+    }
+
+    const activity = { id: newId, ...body }
+    fileWriter("./data/activities.json", activity)
+
+    res.json(activity)
 })
 
 app.listen(PORT)
--- backend/hackathon-data-provider/src/fileHandler.ts
+++ backend/hackathon-data-provider/src/fileHandler.ts
@@ -8,3 +8,16 @@ export const fileHandler = (path: string): Promise<string> => new Promise((resol
             resolve(data.toString())
         }
     }))
+
+export const fileWriter = (path: string, updatedActivity): void => {
+    const x = fs.readFileSync(path).toString();
+    const x_json = JSON.parse(x);
+    const filtered = x_json.filter(activity => activity.id != updatedActivity.id);
+    const updated  = filtered.concat(updatedActivity);
+    const jsonStr  = JSON.stringify(updated, null, 2);
+    fs.writeFile(path, jsonStr, (err) => {
+        if (err) {
+            console.log("ERROR: updating jsonfile,", err);
+        }
+    });
+}
